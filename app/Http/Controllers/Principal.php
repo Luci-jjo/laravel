053a41fce2d4bd77bd8f5d9c0f005e690c423201
Luci-jjo <luccianocraft123@gmail.com>
2026-03-18 18:21:04 -0300

    parametrização de rota

--- app/Http/Controllers/Principal.php
+++ app/Http/Controllers/Principal.php
@@ -10,6 +10,29 @@ class Principal extends Controller
         echo 'Página Principal';
     }
 
+    function contato(string $nome){
+        // echo "O nome do contato é: $nome";
+        $contato = [
+            "nome" => $nome
+        ];
+        return view('contato', $contato);
+    }
+
+    function contatoNomeCompleto(string $nome, string $sobrenome){
+        echo "O nome completo do contato é: $nome $sobrenome";
+    }
+
+    function assunto(string $nome, string $sobrenome, string $assunto){
+        echo "<h1>Contato: $nome $sobrenome </h1>";
+        echo "<marquee> $assunto</marquee>";
+    }
+
+    function mensagem(string $nome, string $sobrenome, string $assunto, string $email, $telefone = 'Telefone não informado'){
+        echo "<h1>Contato: $nome $sobrenome </h1>";
+        echo "<marquee> $assunto</marquee>";
+        echo "Email: $email<br>"; 
+        echo "Telefone: $telefone";
+    }
     
 }
 
--- routes/web.php
+++ routes/web.php
@@ -13,27 +13,22 @@ use App\Http\Middleware\LogAcessoMiddleware;
 |
 */
 
-Route::get('/', [App\Http\Controllers\Principal::class, 'principal']);
-Route::get('/contato', [App\Http\Controllers\Contato::class, 'contato']);
+Route::get('/', [App\Http\Controllers\Principal::class, 'principal'])->name('pagina-inicial');
 
-//ALUNO
 
-Route::get('/aluno', [App\Http\Controllers\Aluno::class, 'aluno']);
-Route::get('/aluno/login', [App\Http\Controllers\Aluno::class, 'aluno_login']);
-Route::get('/aluno/logout', [App\Http\Controllers\Aluno::class, 'aluno_logout']);
-Route::get('/aluno/consultar-nota', [App\Http\Controllers\Aluno::class, 'consultar_nota']);
-Route::get('/aluno/horario-do-curso', [App\Http\Controllers\Aluno::class, 'horario_do_curso']);
-Route::get('/aluno/vida-academica', [App\Http\Controllers\Aluno::class, 'vida_academica']);
-Route::get('/aluno/emitir-documentos', [App\Http\Controllers\Aluno::class, 'emitir_documentos']);
 
-//PROFESSOR
+Route::prefix('publico')->group(function(){
+    Route::get('/contato/{nome}', [App\Http\Controllers\Principal::class, 'contato']);
+    Route::get('/contato/{nome}/{sobrenome}', [App\Http\Controllers\Principal::class, 'contatoNomeCompleto']);
+    Route::get('/contato/{nome}/{sobrenome}/{assunto}', [App\Http\Controllers\Principal::class, 'assunto']);
+    Route::get('/contato/{nome}/{sobrenome}/{assunto}/{email}/{telefone?}', [App\Http\Controllers\Principal::class, 'mensagem']);
+});;
 
-Route::get('/professor', [App\Http\Controllers\Professor::class, 'professor']);
-Route::get('/professor/login', [App\Http\Controllers\Professor::class, 'professor_login']);
-Route::get('/professor/logout', [App\Http\Controllers\Professor::class, 'professor_logout']);
-Route::get('/professor/lancar-nota', [App\Http\Controllers\Professor::class, 'lancar_nota']);
-Route::get('/professor/horario-do-curso', [App\Http\Controllers\Professor::class, 'grade_de_aula']);
-Route::get('/professor/vida-academica', [App\Http\Controllers\Professor::class, 'vida_academica']);
+Route:: fallback(function(){
+    echo "A rota acessada não existe.";
+    echo "<a href='" . route('pagina-inicial') . "'>Voltar</a>";
+});
+
 
 
 
